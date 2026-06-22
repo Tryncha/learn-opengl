170aa69af6fe8dff9dee81271fd35354d81ab9d2
Tryncha <juanes.aguirre26@gmail.com>
2026-06-22 00:01:21 -0500

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,8 @@ const char* fragment_shader_source{
     "}\n\0"};
 }  // namespace shader
 
+// glfw: whenever the window size changed (by OS or user resize) this callback
+// function executes
 // clang-format off
 void framebuffer_size_callback([[maybe_unused]] GLFWwindow* window,
                                int width, int height) {
@@ -34,6 +36,8 @@ void framebuffer_size_callback([[maybe_unused]] GLFWwindow* window,
 }
 // clang-format on
 
+// process all input: query GLFW whether relevant keys are pressed/released this
+// frame and react accordingly
 void process_input(GLFWwindow* window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
     glfwSetWindowShouldClose(window, true);
@@ -41,11 +45,13 @@ void process_input(GLFWwindow* window) {
 }
 
 int main(int, char**) {
+  // glfw: initialize and configure
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  // glfw window creation
   GLFWwindow* window{glfwCreateWindow(constant::width, constant::height,
                                       "LearnOpenGL", NULL, NULL)};
 
@@ -58,16 +64,20 @@ int main(int, char**) {
   glfwMakeContextCurrent(window);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  // glad: load all OpenGL function pointers
   if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
     std::cout << "Failed to initialize GLAD.\n";
     return -1;
   }
 
+  // build and compile our shader program
+  // vertex shader
   unsigned int vertex_shader{};
   vertex_shader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertex_shader, 1, &shader::vertex_shader_source, NULL);
   glCompileShader(vertex_shader);
 
+  // check for shader compile errors
   int success{};
   char info_log[512]{};
   glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
@@ -77,6 +87,7 @@ int main(int, char**) {
               << info_log << '\n';
   }
 
+  // fragment shader
   unsigned int fragment_shader{};
   fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragment_shader, 1, &shader::fragment_shader_source, NULL);
@@ -89,6 +100,7 @@ int main(int, char**) {
               << info_log << '\n';
   }
 
+  // link shaders
   unsigned int shader_program{};
   shader_program = glCreateProgram();
   glAttachShader(shader_program, vertex_shader);
@@ -104,6 +116,7 @@ int main(int, char**) {
   glDeleteShader(vertex_shader);
   glDeleteShader(fragment_shader);
 
+  // set up vertex data (and buffer(s)) and configure vertex attributes
   // clang-format off
   float vertices[]{-0.5f, -0.5f, 0.0f,
                     0.5f, -0.5f, 0.0f,
@@ -115,6 +128,8 @@ int main(int, char**) {
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
 
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s),
+  // and then configure vertex attributes(s)
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -124,27 +139,45 @@ int main(int, char**) {
                         reinterpret_cast<void*>(0));
   glEnableVertexAttribArray(0);
 
+  // note that this is allowed, the call to glVertexAttribPointer registered VBO
+  // as the vertex attribute's bound vertex buffer object so afterwards we can
+  // safely unbind
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // you can unbind the VAO afterwards so other VAO calls won't accidentally
+  // modify this VAO, but this rarely happens.
+  // modifying other VAOs requires a call to glBindVertexArray anyways so we
+  // generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
   glBindVertexArray(0);
 
+  // render loop
   while (!glfwWindowShouldClose(window)) {
+    // input
     process_input(window);
 
+    // render
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // draw our first triangle
     glUseProgram(shader_program);
+    // seeing as we only have a single VAO there's no need to bind it every
+    // time, but we'll do so to keep things a bit more organized
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
+    // glfw: swap buffers and poll IO events
+    // (keys pressed/released, mouse moved, etc.)
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
+  // optional: de-allocate all resources once they've outlived their purpose
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
   glDeleteProgram(shader_program);
 
+  // glfw: terminate, clearing all previously allocated GLFW resources
   glfwTerminate();
   return 0;
 }
