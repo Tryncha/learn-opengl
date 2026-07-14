3d4a2652ed3520f9cadee8aa57e7432a3f18047a
Tryncha <juanes.aguirre26@gmail.com>
2026-07-14 17:25:40 -0500

    add some post-processing effects!

--- 4-advanced-opengl/shaders/screen_fragment.glsl
+++ 4-advanced-opengl/shaders/screen_fragment.glsl
@@ -7,5 +7,19 @@ out vec4 FragColor;
 uniform sampler2D u_ScreenTexture1;
 
 void main() {
-  FragColor = vec4(vec3(1.0 - texture(u_ScreenTexture1, TexCoords)), 1.0);
+  // Normal
+  FragColor = vec4(vec3(texture(u_ScreenTexture1, TexCoords)), 1.0);
+
+  // Inverse
+  // FragColor = vec4(vec3(1.0 - texture(u_ScreenTexture1, TexCoords)), 1.0);
+
+  // Grayscale
+  // FragColor = texture(u_ScreenTexture1, TexCoords);
+  // float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+  // FragColor = vec4(vec3(average), 1.0);
+
+  // "Realistic" grayscale
+  // FragColor = texture(u_ScreenTexture1, TexCoords);
+  // float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+  // FragColor = vec4(vec3(average), 1.0);
 }
