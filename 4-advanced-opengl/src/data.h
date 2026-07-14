b2f88b03a71382e533aa5811eb9eeaa8d4c7918d
Tryncha <juanes.aguirre26@gmail.com>
2026-07-14 15:02:24 -0500

    understanding face culling

--- 4-advanced-opengl/src/data.h
+++ 4-advanced-opengl/src/data.h
@@ -7,50 +7,64 @@
 // clang-format off
 namespace data {
 // vertex data
+
+/*
+Remember: to specify vertices in a counter-clockwise winding order you need
+to visualize the triangle as if you're in front of the triangle and from that
+point of view, is where you set their order.
+
+To define the order of a triangle on the right side of the cube for example,
+you'd imagine yourself looking straight at the right side of the cube, and
+then visualize the triangle and make sure their order is specified in a
+counter-clockwise order. This takes some practice, but try visualizing this
+yourself and see that this is correct.
+*/
+
 // each row corresponds to a vertex:
 // 3 floats, 2 floats -> position, texture coords
 constexpr std::array<float, 5 * 6 * 6> cubeVertices{
-  -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-   0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-  -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-   0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-   0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-   0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-  -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-  -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-  -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-  -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-   0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-   0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-   0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-   0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-   0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-   0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-  -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+  // Back face
+  -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,  // bottom-left
+   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  // top-right
+   0.5f, -0.5f, -0.5f,  1.0f, 0.0f,  // bottom-right         
+   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  // top-right
+  -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,  // bottom-left
+  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,  // top-left
+  // Front face
+  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  // bottom-left
+   0.5f, -0.5f,  0.5f,  1.0f, 0.0f,  // bottom-right
+   0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  // top-right
+   0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  // top-right
+  -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  // top-left
+  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  // bottom-left
+  // Left face
+  -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top-right
+  -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  // top-left
+  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom-left
+  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom-left
+  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  // bottom-right
+  -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top-right
+  // Right face
+   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top-left
+   0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom-right
+   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  // top-right         
+   0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // bottom-right
+   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // top-left
+   0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  // bottom-left     
+  // Bottom face
+  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // top-right
+   0.5f, -0.5f, -0.5f,  1.0f, 1.0f,  // top-left
+   0.5f, -0.5f,  0.5f,  1.0f, 0.0f,  // bottom-left
+   0.5f, -0.5f,  0.5f,  1.0f, 0.0f,  // bottom-left
+  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  // bottom-right
+  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  // top-right
+  // Top face
+  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,  // top-left
+   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // bottom-right
+   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  // top-right     
+   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  // bottom-right
+  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,  // top-left
+  -0.5f,  0.5f,  0.5f,  0.0f, 0.0f   // bottom-left 
 };
 
 // each row corresponds to a vertex:
--- 4-advanced-opengl/src/main.cpp
+++ 4-advanced-opengl/src/main.cpp
@@ -179,8 +179,12 @@ int main(int, char**) {
 
   // Configure global opengl state
   glEnable(GL_DEPTH_TEST);
+
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_FRONT);
 
   // build and compile shaders
   Shader ourShader{
