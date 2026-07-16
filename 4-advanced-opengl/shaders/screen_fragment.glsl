c6a0546aaa92ed579523f9675b9fa69ade58d5d1
Tryncha <juanes.aguirre26@gmail.com>
2026-07-15 19:56:29 -0500

    add more kernels

--- 4-advanced-opengl/shaders/screen_fragment.glsl
+++ 4-advanced-opengl/shaders/screen_fragment.glsl
@@ -20,10 +20,22 @@ const vec2 OFFSETS[9] = vec2[](
   vec2(OFFSET,  -OFFSET)
 );
 
-const float KERNEL[9] = float[](
-  -1, -1, -1,
-  -1,  9, -1,
-  -1, -1, -1
+const float SHARPEN_KERNEL[9] = float[](
+  -1.0, -1.0, -1.0,
+  -1.0,  9.0, -1.0,
+  -1.0, -1.0, -1.0
+);
+
+const float BLUR_KERNEL[9] = float[](
+  1.0 / 16.0, 2.0 / 16.0, 1.0 / 16.0,
+  2.0 / 16.0, 4.0 / 16.0, 2.0 / 16.0,
+  1.0 / 16.0, 2.0 / 16.0, 1.0 / 16.0
+);
+
+const float EDGE_DET_KERNEL[9] = float[](
+  1.0,  1.0, 1.0,
+  1.0, -9.0, 1.0,
+  1.0,  1.0, 1.0
 );
 
 void main() {
@@ -34,7 +46,7 @@ void main() {
 
   vec3 color = vec3(0.0);
   for (int i = 0; i < 9; i++) {
-    color += sampleTex[i] * KERNEL[i];
+    color += sampleTex[i] * BLUR_KERNEL[i];
   }
 
   FragColor = vec4(color, 1.0);
