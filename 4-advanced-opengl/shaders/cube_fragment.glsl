ee7e2861380434b029c353a5647261fff1f1207f
Tryncha <juanes.aguirre26@gmail.com>
2026-07-17 10:22:11 -0500

    refraction add to the scene

--- 4-advanced-opengl/shaders/cube_fragment.glsl
+++ 4-advanced-opengl/shaders/cube_fragment.glsl
@@ -8,9 +8,24 @@ out vec4 FragColor;
 uniform vec3 u_CameraPosition;
 uniform samplerCube u_SkyboxTexture;
 
-void main() {
+const float AIR_REFRACTIVE_INDEX = 1.0;
+const float GLASS_REFRACTIVE_INDEX = 1.52;
+
+vec4 reflectColor() {
   vec3 I = normalize(Position - u_CameraPosition);
   vec3 R = reflect(I, normalize(Normal));
 
-  FragColor = vec4(texture(u_SkyboxTexture, R).rgb, 1.0);
+  return vec4(texture(u_SkyboxTexture, R).rgb, 1.0);
+}
+
+vec4 refractColor() {
+  float ratio = AIR_REFRACTIVE_INDEX / GLASS_REFRACTIVE_INDEX;
+  vec3 I = normalize(Position - u_CameraPosition);
+  vec3 R = refract(I, normalize(Normal), ratio);
+
+  return vec4(texture(u_SkyboxTexture, R).rgb, 1.0);
+}
+
+void main() {
+  FragColor = refractColor();
 }
